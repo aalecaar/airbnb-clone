73fba543524793bbbbc250359aaae8a40b85e903
Alejandro Cardona <alejo.98k@gmail.com>
2024-05-21 17:27:19 -0500

    Add: search bar

--- AirbnbClone/Core/Explore/SearchAndFilterBarView.swift
+++ AirbnbClone/Core/Explore/SearchAndFilterBarView.swift
@@ -10,8 +10,37 @@ import SwiftUI
 struct SearchAndFilterBarView: View {
     var body: some View {
         HStack {
+            Image(systemName: "magnifyingglass")
             
+            VStack(alignment: .leading) {
+                Text("Where to?")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Text("Anywhere - Any Week - Any Guests")
+                    .font(.caption2)
+                    .foregroundStyle(.secondary)
+            }
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "line.3.horizontal.decrease.circle")
+                    .foregroundStyle(.black)
+            }
+           
         }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .overlay {
+            Capsule()
+                .stroke(lineWidth: 0.5)
+                .foregroundStyle(.quaternary)
+                .shadow(color: .gray.opacity(0.4), radius: 2)
+        }
+        
     }
 }
 
